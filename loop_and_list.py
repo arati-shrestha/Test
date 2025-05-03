93b5d101620da3ddc0783c260e3cbbbfc5fdd71a
Arati Shrestha <arati.shrestha3320@gmail.com>
2025-05-03 13:17:03 +0545

    comment change

--- loop_and_list.py
+++ loop_and_list.py
@@ -12,7 +12,7 @@ def convert_to_snake_case(pascal_or_camel_cased_string):
     return ''.join(snake_cased_char_list).strip('_')
 
 def main():
-    text = input("Enter any string in camelCase or PascalCase format:")
+    text = input("Enter any string in camelCase or PascalCase format you silly:")
     print(convert_to_snake_case(text))
 
     
